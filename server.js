802a79a78e64c903b27e4005f05bfa8b04736522
Aleš Kabourek <ales.kabourek@gmail.com>
2024-07-30 17:30:59 +0200

    permission due 'realm role' added

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const config = require('./config.json');
 const app = express();
 const memoryStore = new session.MemoryStore();
 
-const keycloak = new Keycloak({ store: memoryStore });
+const keycloak = new Keycloak({store: memoryStore});
 
 // configure express session
 app.use(session({
@@ -19,12 +19,28 @@ app.use(session({
 
 app.use(keycloak.middleware());
 
+function checkRole(role) {
+  return function (req, res, next) {
+    const token = req.kauth.grant.access_token.content;
+    if (!role) next();
+
+    console.log(token.realm_access.roles);
+    if (token.realm_access.roles.includes(role)){
+      console.log('Approved');
+      next();
+    }else{
+      console.log('Forbidden');
+      res.status(403).send('Forbidden');
+    }
+  };
+}
+
 config.public.forEach((route) => {
   app.use(route.uri, express.static(route.static));
   console.log(`Serving public content ${route.uri} from ${route.static}`);
 });
 config.protected.forEach((route) => {
-  app.use(route.uri, keycloak.protect(), express.static(route.static));
+  app.use(route.uri, keycloak.protect(), checkRole(config?.realmRole ?? null), express.static(route.static));
   console.log(`Serving protected content ${route.uri} from ${route.static}`);
 });
 
